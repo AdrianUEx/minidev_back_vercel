68fa061019ce327816fbd315cd682010e4cdf92c
Adrian <abarquilx@alumnos.unex.es>
2026-08-13 19:38:09 +0200

    Adición de comentarios y de app.get básicos para probar las rutas de Vercel

--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -20,12 +20,17 @@ app.use(cors()); // needs npm i --save-dev @types/cors
 app.get("/", (req: Request, res: Response) => {
   res.send("Express 5.2.1 + TypeScript 5.5.6 backend in Vercel is running");
 });
+app.get("/author", (req: Request, res: Response) => {
+  res.send("Petición GET a /author");
+});
+app.get("/author/:id", (req: Request, res: Response) => {
+  res.send(`Petición GET a /author con id ${req.params.id}`);
+});
 
-export default app;
 
-if (require.main === module) {
-  app.listen(port, () => {
-    // Mandatory to use in order to recieve requests. Without app.listen() it's like the backend is not running, even if the code is being executed.
-    console.log("Express backend in Vercel is running");
-  });
-}
+app.listen(port, () => {
+  // Mandatory to use in order to recieve requests. Without app.listen() it's like the backend is not running, even if the code is being executed.
+  console.log("Express backend in Vercel is running");
+});
+
+export default app; // Necessary to export by default without a name, but also to be able to use this project in Vercel. If you don't export it by default, Vercel won't be able to find it and the Serverless function won't be created, throwing an error on logs.
